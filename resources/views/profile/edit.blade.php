a10b54e214041b5219f7c67eb613021c37c40c52
chadni54 <chadni@eh.studio>
2025-09-26 18:11:04 +0600

    update profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,23 +1,29 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12 px-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 lg:py-12 py-8 px-4">
+        <h2 class="text-2xl font-bold text-gray-900 mb-6">User Profile Information</h2>
+       <div class="">
+        <div class=" mx-auto   space-y-6">
+            
+            <!-- Profile Information -->
+            <div class="p-6 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl mx-auto">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <!-- Update Password -->
+            <div class="p-6 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl mx-auto">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
         </div>
     </div>
+    </div>
+
+
+
+
+  
 </x-app-layout>
